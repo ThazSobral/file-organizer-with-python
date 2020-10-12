241d019742ec701c3f2701bf56bfab090d07b815
Thalles Sobral <thazsobral@gmail.com>
2020-10-11 22:15:04 -0300

    class added to show logo and choose the extensions

--- file-organizer.py
+++ file-organizer.py
@@ -3,7 +3,18 @@ import shutil
 from datetime import datetime
 
 class Organizer:
-    extensions = ['jpg', 'jpeg', 'JPG', 'JPEG', 'png', 'PNG', 'pdf', 'mp4']
+    def organize(self, extensions):
+        photos = [
+            filename for filename in os.listdir('.') if any(filename.endswith(ext) for ext in extensions)
+        ]
+        for filename in photos:
+            self.move_photo(filename)
+    
+    def move_photo(self, file):
+        new_folder = self.folder_path_from_photo_date(file)
+        if not os.path.exists(new_folder):
+            os.makedirs(new_folder)
+        shutil.move(file, new_folder + '/' + file)
 
     def folder_path_from_photo_date(self, file):
         date = self.photo_shooting_date(file)
@@ -13,18 +24,26 @@ class Organizer:
         date = datetime.fromtimestamp(os.path.getmtime(file))
         return date
 
-    def move_photo(self, file):
-        new_folder = self.folder_path_from_photo_date(file)
-        if not os.path.exists(new_folder):
-            os.makedirs(new_folder)
-        shutil.move(file, new_folder + '/' + file)
+class Manager:
+    extensions = []
+    def __init__(self):
+        self.intro()
+        self.extensions = self.extensions_questions()
 
-    def organize(self):
-        photos = [
-            filename for filename in os.listdir('.') if any(filename.endswith(ext) for ext in self.extensions)
-        ]
-        for filename in photos:
-            self.move_photo(filename)
+    def intro(self):
+        print('\
+        ooooo  ooooo  ooooo  ooooo  o    o  o  ooooo  ooooo  ooooo\n\
+        o   o  o   o  o      o   o  o o  o  o     o   o      o   o\n\
+        o   o  ooooo  o ooo  o   o  o  o o  o    o    ooooo  ooooo\n\
+        o   o  o  o   o   o  ooooo  o   oo  o   o     o      o  o \n\
+        ooooo  o   o  ooooo  o   o  o    o  o  ooooo  ooooo  o   o\n\
+        \nOrganize seus arquivos do seu jeito.\n\
+        ')
 
-PO = Organizer()
-PO.organize()
+    def extensions_questions(self):
+        extensions = input('Com quais extensões deseja trabalhar? \n(ex: jpg, pdf ...)\n')
+        return extensions.replace(" ", "").split(',')
+
+config = Manager()
+organizer = Organizer()
+organizer.start(config.extensions)
